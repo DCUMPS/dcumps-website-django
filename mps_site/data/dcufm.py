fefe4829d9b52b7bc718802dcd9a7646570b2c53
Jake Farrell <jake.farrell88@mail.dcu.ie>
2025-02-08 02:21:25 +0000

    added member fm shows to website

--- mps_site/data/dcufm.py
+++ mps_site/data/dcufm.py
@@ -127,6 +127,106 @@ dcufm_shows = {
         {"url": "https://www.instagram.com/theoriginalsindcufm/", "icon_class": "fa-brands fa-instagram"},
         {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
       ]
+    },
+    {
+      "name": "Evan’s Double Entendre",
+      "names": ["Evan Dalton"],
+      "image": "images/shows/de.png",
+      "alt_text": "DCUfm Show",
+      "description": "The show where music means more!<br>Wednesday 9AM",
+      "social_links": [
+        {"url": "https://www.tiktok.com/@evandaltoncommentary?_t=ZN-8tMOReWG03S&_r=1", "icon_class": "fa-brands fa-tiktok"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "The Grandstand Sports Show",
+      "names": ["Evan Dalton", "Tiarnan O Kelly"],
+      "image": "images/shows/gs.png",
+      "alt_text": "DCUfm Show",
+      "description": "Sports opinions from within the crowd!<br>Wednesday 12PM",
+      "social_links": [
+        {"url": "https://open.spotify.com/show/6XhnLjhHCHENRl2Uuzm5f6?si=jUWeALb7Qj-akONAcCrl7w", "icon_class": "fa-brands fa-spotify"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "Show Ate",
+      "names": ["Kaitlyn Firmo"],
+      "image": "images/shows/sa.jpeg",
+      "alt_text": "DCUfm Show",
+      "description": "Bite size lifestyle show<br>Wednesday at 10AM",
+      "social_links": [
+        {"url": "https://www.instagram.com/kaitlyndfxxox", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "Soundwaves with Sarah and Sophie",
+      "names": ["Sarah Duff", "Sophie-Elizabeth King"],
+      "image": "images/shows/sw.jpeg",
+      "alt_text": "DCUfm Show",
+      "description": "We talk all things music, pop culture and more!<br>Fridays at 1PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/soundwavesdcufm", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "Fresh Princes On Air",
+      "names": ["Beth O'Connor", "Shaney McConnon", "Jake Dalton", "Paul Farrell"],
+      "image": "images/shows/fp.jpeg",
+      "alt_text": "DCUfm Show",
+      "description": "Keep your mornings fresh with the fresh princes<br>Monday 12-1PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/fresh.princes.on.air", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "Scene-It!",
+      "names": ["Iara Moreira", "Aria Kazi", "Eva Mountfort"],
+      "image": "images/shows/si.png",
+      "alt_text": "DCUfm Show",
+      "description": "We watch movies so you don't have to.<br>Wednesday 11AM-12PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/scene_it_dcufm", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "ChitChatFM",
+      "names": ["Emily Mullally", "Ciara Stell", "Clodagh Mahon"],
+      "image": "images/shows/cc.png",
+      "alt_text": "DCUfm Show",
+      "description": "Your three favourite Communications students with a talent for chatting ;)<br>Friday 2-3PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/chitchatfm", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.tiktok.com/@chitchatfm?_t=ZN-8tN1fmbXdtW&_r=1", "icon_class": "fa-brands fa-tiktok"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+    {
+      "name": "DCYouWantTheNews",
+      "names": ["Aaron Casey", "Ailish Connor"],
+      "image": "images/shows/ac.jpg",
+      "alt_text": "DCUfm Show",
+      "description": "All the news you didn't know you wanted but absolutely did NOT need.<br>Wednesday: 3-4PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/dcyouwantthenews", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
+    },
+     {
+      "name": "This & Yap",
+      "names": ["Mya Breen", "Olivia Doyle"],
+      "image": "images/shows/ty.png",
+      "alt_text": "DCUfm Show",
+      "description": "Yapping every Monday @ 3PM<br>Mondays 3-4PM",
+      "social_links": [
+        {"url": "https://www.instagram.com/thisandyap_?igsh=MW55cG8xa2d1Z2k1bw%3D%3D&utm_source=qr", "icon_class": "fa-brands fa-instagram"},
+        {"url": "https://www.twitch.tv/dcufm", "icon_class": "fa-brands fa-twitch"}
+      ]
     }
   ]
 }
